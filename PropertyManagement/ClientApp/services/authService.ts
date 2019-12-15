0fc3d5ae237411ff2d399ad77546d9d81eb660d6
Nico <nicola.dilillo@icloud.com>
2019-12-15 00:51:30 +0000

    Add project files.

---
+++ PropertyManagement/ClientApp/services/authService.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';  // https://github.com/auth0/angular2-jwt
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = environment.apiUrl;
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
+  currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          localStorage.setItem('user', JSON.stringify(user.user));
+          this.currentUser = user.user;
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        }
+      })
+    );
+  }
+
+  register(user: User) {
+    return this.http.post(this.baseUrl + 'register', user);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+  logout() {
+    localStorage.removeItem('token');
+    this.decodedToken = null;
+
+    localStorage.removeItem('user');
+    this.currentUser = null;
+  }
+}
+
